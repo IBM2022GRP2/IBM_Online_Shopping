88b25d0cf86034d7d9d38509267341ba0034fe0a
Mrinal-delta <i.mrinal.samanta@gmail.com>
2022-04-04 21:36:17 +0530

    Product Repo rectified

--- online-shopping/src/main/java/com/ibm/repo/ProductRepository.java
+++ online-shopping/src/main/java/com/ibm/repo/ProductRepository.java
@@ -8,15 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.ibm.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer>{
-<<<<<<< HEAD
-=======
-
-	
-@Query("FROM Product WHERE price>=:lowp AND price<=:highp")	
-List<Product> findByPriceRange(double lowp,double highp);	
-	
-List<Product>findByCategory(String category) ;
->>>>>>> 144dadc027710f75bc1195961d6ba920472aab2c
 
 	@Query("FROM Product WHERE price>=:lowp AND price<=:highp")	
 	List<Product> findByPriceRange(double lowp,double highp);	
