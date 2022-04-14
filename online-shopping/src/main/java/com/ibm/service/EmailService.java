0f1c7e94cfe0283227efc73b5a66113f878dd2e9
cap-codeDeb <debarghyadutta36@gmail.com>
2022-04-14 15:51:02 +0530

    Email golpo added

---
+++ online-shopping/src/main/java/com/ibm/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.ibm.service;
+
+public interface EmailService {
+	
+	public void sendEmail(String receiver, String subject, String body);
+
+}
--- online-shopping/src/main/java/com/ibm/service/UserServiceImpl.java
+++ online-shopping/src/main/java/com/ibm/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.ibm.entity.User;
 import com.ibm.repo.UserRepository;
+import com.ibm.service.EmailServiceImpl;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,9 +15,18 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository repo;
 	
+	@Autowired
+	private EmailServiceImpl emailservice;
+	
 	@Override
 	public int save(User u) {
 		repo.save(u);
+//		emailservice.setReceiver(u.getEmail());
+//		emailservice.setBody("Hi "+u.getUsername()+" ! \n Welcome to our webiste Fragnance World. " );
+//		emailservice.setSubject("Welcome to Fragnance World");
+		String msg = "Hi "+u.getUsername()+" ! \n Welcome to our webiste Fragnance World. " ;
+		String sub= "Welcome to Fragnance World";
+		emailservice.sendEmail(u.getEmail(),sub,msg);
 		return u.getUserId();
 	}
 
